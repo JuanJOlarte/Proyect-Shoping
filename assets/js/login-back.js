4161f1361b5f1a9a9b944995e4a05804c525c9c6
juan olarte <juan.olartex64@gmail.com>
2024-06-25 18:36:28 -0500

    regreso a pagina index desde login mediante js

---
+++ assets/js/login-back.js
@@ -0,0 +1,8 @@
+const btnCambiarPagina = document.getElementById('btnCambiarPagina');
+const i=4;
+console.log(btnCambiarPagina);
+console.log(i);
+btnCambiarPagina.addEventListener('click', function() {
+window.location.href = './../../index.html';
+console.log("hola")
+});
